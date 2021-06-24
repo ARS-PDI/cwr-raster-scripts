0d80180e45323a09474727291b4a7a28170387e6
Josh Birlingmair <jcbmair@gmail.com>
2021-06-24 11:20:42 -0600

    Finished create file GDB and mosaic dataset functions

--- pdi-raster-mapping-script/map-rasters.py
+++ pdi-raster-mapping-script/map-rasters.py
@@ -1,22 +1,32 @@
+import os
+import sys
 import arcpy
 import arcgis
 
-DEBUG = True
-
 if __name__ == '__main__':
-    if DEBUG:
+    if len(sys.argv) != 2:
+        print('Usage: python3 map-rasters.py [workspace]')
         exit()
 
-    # 1. TODO: Add/access cloud storage connection?
-    # 2. TODO: Create new file Geodatabase in project
-    arcpy.CreateFileGDB_management('path', 'fgdb.gdb')
-    # 3. TODO: Create new mosaic dataset in file Geodatabase
-    #    - WGS_1984_Web_Mercator_Auxiliary_Sphere coordinate system
-    arcpy.CreateMosaicDataset_management()
-    # 4. TODO: Add raster file to dataset
-    arcpy.AddRastersToMosaicDataset_management()
-    # 5. TODO: Create raster function template
-    #    - Raster -> Remap
+    arcpy.env.workspace = workspace = sys.argv[1]
+    fgdb = 'workspace.gdb'
+
+    # TODO?: Add/access cloud storage connection
+
+    # Create new file Geodatabase in project
+    arcpy.CreateFileGDB_management(workspace, fgdb)
+
+    # Create new mosaic dataset in file Geodatabase
+    # - WGS_1984_Web_Mercator_Auxiliary_Sphere coordinate system
+    coord_sys = arcpy.SpatialReference('WGS 1984 Web Mercator Auxiliary Sphere')
+    arcpy.CreateMosaicDataset_management(workspace, 'test-mosaic', coord_sys, 1)
+
+    # TODO: Add raster file to dataset
+    dataset_path = os.path.join(workspace, fgdb)
+    input_path = os.path.join(workspace) # FIXME
+    arcpy.AddRastersToMosaicDataset_management(dataset_path, 'Raster Dataset', input_path)
+
+    # TODO: Raster -> Remap
     arcgis.raster.remap()
-    #    - Remap -> Attribute table
-    #    - Page 8
+
+    # TODO: Remap -> Attribute table (Page 8)

9dbafbaf29df145c3cb21cbb578e1bb590f6c68e
Josh Birlingmair <jcbmair@gmail.com>
2021-06-24 17:07:11 -0600

    Minor changes and progress on remap function

--- pdi-raster-mapping-script/map-rasters.py
+++ pdi-raster-mapping-script/map-rasters.py
@@ -10,6 +10,7 @@ if __name__ == '__main__':
 
     workspace = sys.argv[1]
     mrf_folder = sys.argv[2]
+    test_mosaic = 'test_mosaic'
     fgdb = 'workspace.gdb'
 
     # TODO?: Add/access cloud storage connection
@@ -21,14 +22,17 @@ if __name__ == '__main__':
     # - WGS_1984_UTM_Zone_14N coordinate system
     arcpy.env.workspace = path.join(workspace, fgdb)
     coord_sys = arcpy.SpatialReference('WGS 1984 UTM Zone 14N')
-    arcpy.CreateMosaicDataset_management(arcpy.env.workspace, 'test_mosaic', coord_sys, 1)
+    arcpy.CreateMosaicDataset_management(arcpy.env.workspace, test_mosaic, coord_sys, 1)
 
-    # TODO: Add raster file to dataset
-    dataset_path = path.join(arcpy.env.workspace, 'test_mosaic')
+    # Add raster file to dataset
+    dataset_path = path.join(arcpy.env.workspace, test_mosaic)
     input_path = path.join(workspace, mrf_folder) # FIXME
     arcpy.AddRastersToMosaicDataset_management(dataset_path, 'Raster Dataset', input_path)
 
     # TODO: Raster -> Remap
-    # arcgis.raster.remap()
+    curr_rast = path.join(arcpy.env.workspace, test_mosaic)
+    input_ranges = [1, 10, 20]
+    output_ranges = [10, 20, 30]
+    arcgis.raster.functions.remap(test_mosaic, input_ranges, output_ranges)
 
     # TODO: Remap -> Attribute table (Page 8)

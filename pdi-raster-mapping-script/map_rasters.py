fc66d9bc6170570f60fe116d81e6f925bc0bd9be
Josh Birlingmair <jcbmair@gmail.com>
2021-06-29 10:37:06 -0600

    Drafted recursive version of raster mapping script

--- pdi-raster-mapping-script/map_rasters.py
+++ pdi-raster-mapping-script/map_rasters.py
@@ -1,35 +1,51 @@
+import os
 import sys
 import arcpy
-from os import path
+
+def map_rasters(input_dir, workspace, rast_func, fgdb):
+    # arg 1: Input directory w/ MRFs -- input_dir
+    for file in os.listdir(input_dir):
+        arcpy.env.workspace = workspace
+
+        if os.path.isdir(os.path.join(input_dir, file)):
+            map_rasters(os.path.join(input_dir, file), workspace, rast_func, fgdb)
+        elif file.endswith('.mrf'):
+            # Create new mosaic dataset in file Geodatabase
+            mosaic = file.replace('.mrf', '')
+            arcpy.CreateMosaicDataset_management(os.path.join(arcpy.env.workspace,
+                                                              fgdb),
+                                                 mosaic,
+                                                 coord_sys,
+                                                 1)
+
+            # Generate new raster from custom raster function
+            arcpy.GenerateRasterFromRasterFunction_management(
+                os.path.join(os.getcwd(), rast_func),
+                os.path.join(arcpy.env.workspace, fgdb, file),
+                'Raster ' + os.path.join(input_dir, file),
+                format='MRF')
+
+            # Add raster file to dataset
+            dataset_path = os.path.join(arcpy.env.workspace, fgdb, mosaic)
+            raster = os.path.join(os.path.join(input_dir, file))
+            arcpy.AddRastersToMosaicDataset_management(dataset_path, 'Raster Dataset', raster)
 
 if __name__ == '__main__':
     if len(sys.argv) != 3:
-        print('Usage: python3 map-rasters.py [workspace] [folder with MRF files]')
+        print('Usage: python3 map_rasters.py [root folder] [raster function file]')
         exit()
 
-    workspace = sys.argv[1]
-    mrf_folder = sys.argv[2]
-    test_mosaic = 'test_mosaic'
-    test_raster = 'grs_pa_PAs_narea_areakm2.mrf'
-    test_function = 'test_function.rft.xml'
-    fgdb = 'workspace.gdb'
+    root_dir = sys.argv[1]
+    rast_func = sys.argv[2]
+    fgdb = 'mosaics.gdb'
 
-    # Create new file Geodatabase in project
-    arcpy.CreateFileGDB_management(workspace, fgdb)
+    # Create new file Geodatabase in current directory and set environment variables
+    try:
+        arcpy.CreateFileGDB_management(os.getcwd(), fgdb)
+    except:
+        pass
 
-    # Create new mosaic dataset in file Geodatabase
-    arcpy.env.workspace = path.join(workspace, fgdb)
     coord_sys = arcpy.SpatialReference('WGS 1984 UTM Zone 14N')
-    arcpy.CreateMosaicDataset_management(arcpy.env.workspace, test_mosaic, coord_sys, 1)
 
-    # Add raster file to dataset
-    dataset_path = path.join(arcpy.env.workspace, test_mosaic)
-    input_path = path.join(workspace, mrf_folder)
-    arcpy.AddRastersToMosaicDataset_management(dataset_path, 'Raster Dataset', input_path)
-
-    # Generate new raster from custom raster function
-    arcpy.GenerateRasterFromRasterFunction_management(
-        path.join(workspace, test_function),
-        path.join(workspace, test_raster),
-        'Raster ' + path.join(input_path, test_raster),
-        format='MRF')
+    # Map rasters using custom raster function file and put them into file geodatabase
+    map_rasters(root_dir, arcpy.env.workspace, rast_func)

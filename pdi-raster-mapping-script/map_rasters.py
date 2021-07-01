9f37ea914bc90636cdf3e5df46a04567f7fe4904
Josh Birlingmair <jcbmair@gmail.com>
2021-07-01 16:53:37 -0600

    Raster mapping script creates file GDB for each subfolder

--- pdi-raster-mapping-script/map_rasters.py
+++ pdi-raster-mapping-script/map_rasters.py
@@ -2,28 +2,42 @@ import os
 import sys
 import arcpy
 
+def stdize(string):
+    return string.replace(' ', '_').replace('-', '_').replace('.', '')
+
 def map_rasters(input_dir, workspace, rast_func, fgdb):
+    fgdb_path = os.path.join(workspace, 'GDB', fgdb)
+
     for file in os.listdir(input_dir):
         if os.path.isdir(os.path.join(input_dir, file)):
+            for mem in os.listdir(os.path.join(input_dir, file)):
+                if '.mrf' in mem:
+                    fgdb = stdize(file)
+                    fgdb = fgdb + '.gdb'
+                    
+                    arcpy.CreateFileGDB_management(
+                        os.path.join(workspace, 'GDB'),
+                        fgdb
+                    )
+
+                    break
+
             map_rasters(os.path.join(input_dir, file), workspace, rast_func, fgdb)
         elif file.endswith('.mrf'):
             input_rast_path = os.path.join(input_dir, file)
             print('Processing', input_rast_path)
 
-            mosaic = file
-            mosaic = mosaic.replace('.mrf', '')
-            mosaic = mosaic.replace(' ', '_')
-            mosaic = mosaic.replace('-', '_')
-            mosaic = mosaic.replace('.', '')
+            mosaic = file.replace('.mrf', '')
+            mosaic = stdize(mosaic)
 
             # Create new mosaic dataset in file Geodatabase
             arcpy.CreateMosaicDataset_management(
-                os.path.join(arcpy.env.workspace, fgdb),          # Path to new mosaic
+                fgdb_path,                                        # Path to new mosaic
                 mosaic,                                           # Mosaic name
-                arcpy.SpatialReference('WGS 1984 UTM Zone 14N')   # Coordinate system
+                arcpy.SpatialReference('WGS 1984')                # Coordinate system
             )
 
-            mosaic_path = os.path.join(arcpy.env.workspace, fgdb, mosaic)
+            mosaic_path = os.path.join(fgdb_path, mosaic)
             arcpy.AddRastersToMosaicDataset_management(
                 mosaic_path,                                      # Target mosaic dataset
                 'Raster Dataset',                                 # Raster type
@@ -44,17 +58,16 @@ if __name__ == '__main__':
 
     root_dir = sys.argv[1]
     rast_func = sys.argv[2]
-    fgdb = 'mosaics.gdb'
 
     # Set environment variables
     arcpy.env.workspace = os.getcwd()
-    arcpy.env.overwriteOutput = False
+    arcpy.env.overwriteOutput = True
 
     # Create new file Geodatabase in current directory and set environment variables
     try:
-        arcpy.CreateFileGDB_management(os.getcwd(), fgdb)
-    except:
+        os.mkdir('GDB')
+    except FileExistsError:
         pass
 
     # Map rasters using custom raster function file and put them into file geodatabase
-    map_rasters(root_dir, arcpy.env.workspace, rast_func, fgdb)
+    map_rasters(root_dir, arcpy.env.workspace, rast_func, 'default.gdb')

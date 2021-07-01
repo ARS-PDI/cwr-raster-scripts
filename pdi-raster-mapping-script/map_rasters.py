a454bd5de0193cb1e8f9a33eca8fff23e8f9c94d
Josh Birlingmair <jcbmair@gmail.com>
2021-07-01 09:36:44 -0600

    Improved raster mapping script

--- pdi-raster-mapping-script/map_rasters.py
+++ pdi-raster-mapping-script/map_rasters.py
@@ -8,8 +8,13 @@ def map_rasters(input_dir, workspace, rast_func, fgdb):
             map_rasters(os.path.join(input_dir, file), workspace, rast_func, fgdb)
         elif file.endswith('.mrf'):
             input_rast_path = os.path.join(input_dir, file)
-            mosaic = file.replace('.mrf', '')
-            mosaic_path = os.path.join(arcpy.env.workspace, fgdb, mosaic)
+            print('Processing', input_rast_path)
+
+            mosaic = file
+            mosaic = mosaic.replace('.mrf', '')
+            mosaic = mosaic.replace(' ', '_')
+            mosaic = mosaic.replace('-', '_')
+            mosaic = mosaic.replace('.', '')
 
             # Create new mosaic dataset in file Geodatabase
             arcpy.CreateMosaicDataset_management(
@@ -18,11 +23,7 @@ def map_rasters(input_dir, workspace, rast_func, fgdb):
                 arcpy.SpatialReference('WGS 1984 UTM Zone 14N')   # Coordinate system
             )
 
-            try:
-                os.mkdir(os.path.join(workspace, 'output'))
-            except FileExistsError:
-                pass
-
+            mosaic_path = os.path.join(arcpy.env.workspace, fgdb, mosaic)
             arcpy.AddRastersToMosaicDataset_management(
                 mosaic_path,                                      # Target mosaic dataset
                 'Raster Dataset',                                 # Raster type
@@ -47,10 +48,13 @@ if __name__ == '__main__':
 
     # Set environment variables
     arcpy.env.workspace = os.getcwd()
-    arcpy.env.overwriteOutput = True
+    arcpy.env.overwriteOutput = False
 
     # Create new file Geodatabase in current directory and set environment variables
-    arcpy.CreateFileGDB_management(os.getcwd(), fgdb)
+    try:
+        arcpy.CreateFileGDB_management(os.getcwd(), fgdb)
+    except:
+        pass
 
     # Map rasters using custom raster function file and put them into file geodatabase
     map_rasters(root_dir, arcpy.env.workspace, rast_func, fgdb)

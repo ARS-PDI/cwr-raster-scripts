86ba7a6e518c266f4ecbbb7ac2f55b3ad424064c
Josh Birlingmair <jcbmair@gmail.com>
2021-09-23 10:11:59 -0600

    Update publishing script to try to upload 3 times

--- pdi-raster-mapping-script/publish_layers.py
+++ pdi-raster-mapping-script/publish_layers.py
@@ -8,41 +8,76 @@ def publish_layers(workspace):
     """
     for gdb in os.listdir(workspace):
         arcpy.env.workspace = gdb_path = os.path.join(workspace, gdb)
+        arcpy.env.overwriteOutput = False
 
-        for mos in arcpy.ListDatasets(feature_type='Mosaic'):
+        max_tries = 3
+        mos_datasets = arcpy.ListDatasets(feature_type='Mosaic')
+        mos_datasets_len = 0
+
+        try:
+            mos_datasets_len = len(mos_datasets)
+        except:
+            continue
+
+        for i in range(mos_datasets_len):
+            mos = mos_datasets[i]
             sd_draft = os.path.join(workspace, mos + '.sddraft')
             sd = os.path.join(workspace, mos + '.sd')
 
-            try:
-                arcpy.CreateImageSDDraft(
-                    os.path.join(gdb_path, mos),
-                    sd_draft,
-                    mos,
-                    'ARCGIS_SERVER',
-                    copy_data_to_server=True,
-                    folder_name='CWR',
-                    summary=mos,
-                    tags='ARS, CWR'
-                )
+            for curr_try in range(max_tries):
+                try:
+                    arcpy.CreateImageSDDraft(
+                        os.path.join(gdb_path, mos),
+                        sd_draft,
+                        mos,
+                        'ARCGIS_SERVER',
+                        copy_data_to_server=True,
+                        folder_name='CWR',
+                        summary=mos,
+                        tags='ARS, CWR'
+                    )
 
-                arcpy.StageService_server(sd_draft, sd)
+                    break
+                except:
+                    pass
 
-                print('Uploading the service definition for', mos)
-                arcpy.UploadServiceDefinition_server(
-                    sd,
-                    'https://pdiimagery.azurecloudgov.us/arcgis',
-                    in_my_contents=True,
-                    in_public=True,
-                    in_organization='SHARE_ORGANIZATION'
-                )
-                print('Service successfully published')
-            except:
+            for curr_try in range(max_tries):
+                try:
+                    arcpy.StageService_server(sd_draft, sd)
+
+                    break
+                except:
+                    pass
+
+            success = False
+
+            for curr_try in range(max_tries):
+                try:
+                    print(f'[{curr_try + 1}] Uploading the service definition for', mos)
+
+                    arcpy.UploadServiceDefinition_server(
+                        sd,
+                        'https://pdiimagery.azurecloudgov.us/arcgis',
+                        in_my_contents=True,
+                        in_public=True,
+                        in_organization='SHARE_ORGANIZATION'
+                    )
+
+                    print(f'{mos} successfully published')
+                    success = True
+                    
+                    break
+                except:
+                    pass
+
+            if not success:
                 with open('publish_log.txt', 'a') as f:
                     f.write(f'{mos}\n')
-            finally:
-                # Clean up service definitions
-                os.remove(os.path.join(workspace, f'{mos}.sddraft'))
-                os.remove(os.path.join(workspace, f'{mos}.sd'))
+
+            # Clean up service definitions
+            print('Removing', mos, 'service definitions')
+            os.remove(os.path.join(workspace, f'{mos}.sddraft'))
+            os.remove(os.path.join(workspace, f'{mos}.sd'))
 
 if __name__ == '__main__':
     arcpy.SignInToPortal(

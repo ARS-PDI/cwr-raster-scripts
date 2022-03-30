4e9342d63c0771d66ce04f9684b9ad9137620016
Josh Birlingmair <jcbmair@gmail.com>
2022-03-30 10:06:07 -0600

    Cut out some unncecessary lines from map_rasters

--- map_rasters.py
+++ map_rasters.py
@@ -23,72 +23,36 @@ def map_rasters(input_dir, workspace, rast_funcs, fgdb):
     fgdb_path = os.path.join(workspace, 'GDB', fgdb)
 
     for file in os.listdir(input_dir):
-        if os.path.isdir(os.path.join(input_dir, file)):
-            for f in os.listdir(os.path.join(input_dir, file)):
-                if '.mrf' in f:
-                    fgdb = stdize(file)
-                    fgdb = fgdb + '.gdb'
+        input_rast_path = os.path.join(input_dir, file)
+        local_time = time.localtime()
+        print(time.strftime('[%I:%M:%S]', local_time), input_rast_path)
 
-                    if not os.path.exists(os.path.join(workspace, 'GDB', fgdb)):
-                        arcpy.CreateFileGDB_management(
-                            os.path.join(workspace, 'GDB'),
-                            fgdb
-                        )
+        mosaic = file.replace('.mrf', '')
+        mosaic = stdize(mosaic)
 
-                    break
+        rast_func = None
+        if 'ga50' in file:
+            rast_func = rast_funcs['ga50']
+        elif 'grsEx' in file:
+            rast_func = rast_funcs['grsEx']
+        elif 'grsIn_proAreas' in file:
+            rast_func = rast_funcs['grsIn_proAreas']
+        elif 'thrsld_median' in file:
+            rast_func = rast_funcs['thrsld_median']
 
-            map_rasters(os.path.join(input_dir, file),
-                        workspace, rast_funcs, fgdb)
-        elif file.endswith('.mrf'):
-            input_rast_path = os.path.join(input_dir, file)
-            local_time = time.localtime()
-            print(time.strftime('[%I:%M:%S]', local_time), input_rast_path)
+        mosaic_path = os.path.join(fgdb_path, mosaic)
+        arcpy.AddRastersToMosaicDataset_management(
+            mosaic_path,                                      # Target mosaic dataset
+            'Raster Dataset',                                 # Raster type
+            input_rast_path                                   # Path to input raster
+        )
 
-            mosaic = file.replace('.mrf', '')
-            mosaic = stdize(mosaic)
-
-            # Figure out raster function
-            rast_func = None
-            if 'ga50' in file:
-                rast_func = rast_funcs['ga50']
-            elif 'grsEx' in file:
-                rast_func = rast_funcs['grsEx']
-            elif 'grsIn_proAreas' in file:
-                rast_func = rast_funcs['grsIn_proAreas']
-            elif 'thrsld_median' in file:
-                rast_func = rast_funcs['thrsld_median']
-            elif 'ecos' in file:
-                rast_func = rast_funcs['ecos']
-
-            if not rast_func:
-                continue
-
-            # Create new mosaic dataset in file Geodatabase
-            try:
-                arcpy.CreateMosaicDataset_management(
-                    fgdb_path,                                    # Path to new mosaic
-                    mosaic,                                       # Mosaic name
-                    # Coordinate system
-                    arcpy.SpatialReference('WGS 1984')
-                )
-            except KeyboardInterrupt:
-                exit(0)
-            except:
-                pass
-
-            mosaic_path = os.path.join(fgdb_path, mosaic)
-            arcpy.AddRastersToMosaicDataset_management(
-                mosaic_path,                                      # Target mosaic dataset
-                'Raster Dataset',                                 # Raster type
-                input_rast_path                                   # Path to input raster
-            )
-
-            rast_func_path = os.path.join(workspace, rast_func)
-            arcpy.SetMosaicDatasetProperties_management(
-                mosaic_path,                                      # Target mosaic dataset
-                processing_templates=rast_func_path,              # Processing templates
-                default_processing_template=rast_func_path        # Default template
-            )
+        rast_func_path = os.path.join(workspace, rast_func)
+        arcpy.SetMosaicDatasetProperties_management(
+            mosaic_path,                                      # Target mosaic dataset
+            processing_templates=rast_func_path,              # Processing templates
+            default_processing_template=rast_func_path        # Default template
+        )
 
 
 if __name__ == '__main__':

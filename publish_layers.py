0e4ba65303c07254985de1cfb70266091b60c07a
Josh Birlingmair <jcbmair@gmail.com>
2022-04-01 12:25:56 -0600

    Wrap upload process in try/except/finally block

--- publish_layers.py
+++ publish_layers.py
@@ -11,25 +11,28 @@ def publish_layers():
         sd_draft = f'{mosaic}.sddraft'
         sd = f'{mosaic}.sd'
 
-        arcpy.CreateImageSDDraft(mosaic,
-                                 sd_draft,
-                                 f'cwr_{mosaic}',
-                                 'ARCGIS_SERVER',
-                                 copy_data_to_server=True,
-                                 folder_name='CWR',
-                                 summary=mosaic,
-                                 tags=f'CWR,{mosaic},ARS,PDI')
+        try:
+            arcpy.CreateImageSDDraft(mosaic,
+                                    sd_draft,
+                                    f'cwr_{mosaic}',
+                                    'ARCGIS_SERVER',
+                                    copy_data_to_server=True,
+                                    folder_name='CWR',
+                                    summary=mosaic,
+                                    tags=f'CWR,{mosaic},ARS,PDI')
 
-        arcpy.StageService_server(sd_draft, sd)
+            arcpy.StageService_server(sd_draft, sd)
 
-        arcpy.UploadServiceDefinition_server(sd,
-                                             'https://pdiimagery.azurecloudgov.us/arcgis',
-                                             in_my_contents=True,
-                                             in_public=True,
-                                             in_organization='SHARE_ORGANIZATION')
-
-        os.remove(os.path.join(os.getcwd(), sd_draft))
-        os.remove(os.path.join(os.getcwd(), sd))
+            arcpy.UploadServiceDefinition_server(sd,
+                                                'https://pdiimagery.azurecloudgov.us/arcgis',
+                                                in_my_contents=True,
+                                                in_public=True,
+                                                in_organization='SHARE_ORGANIZATION')
+        except arcpy.ExecuteError as e:
+            raise e
+        finally:
+            os.remove(os.path.join(os.getcwd(), sd_draft))
+            os.remove(os.path.join(os.getcwd(), sd))
 
 
 if __name__ == '__main__':

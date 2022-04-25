7af1a929862d9f068dbb8756f760b0a1d0182333
Josh Birlingmair <jcbmair@gmail.com>
2022-04-25 10:41:53 -0600

    Add main script to run through all main functions

---
+++ main.py
@@ -0,0 +1,17 @@
+from copy_rasters import main as cr_main
+from create_mosaics import main as cm_main
+from publish_layers import main as pl_main
+
+def main():
+    cr_input_dir = r'C:\CWR\srcSpeciesLevelData'
+    cr_output_dir = r'C:\CWR\stagingSpeciesLevelData'
+    cr_main(cr_input_dir, cr_output_dir)
+
+    cm_input_dir = r'C:\CWR\stagingSpeciesLevelData'
+    cm_main(cm_input_dir)
+
+    cl_workspace = r'C:\CWR\CWR.gdb'
+    pl_main(cl_workspace)
+
+if __name__ == '__main__':
+    main()
